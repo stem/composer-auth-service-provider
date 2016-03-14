912509eb6b469559618a273b37e5bb6508708a68
Stephane CRIVISIER <stephane@crivisier.fr>
2016-03-14 19:18:53 +0100

    tmp

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -23,9 +23,20 @@ class AuthServiceProvider implements ServiceProviderInterface, BootableProviderI
     /**
      * @{inherit doc}
      */
+    public function register(Container $app)
+    {
+        $app["auth"]                   = $this;
+        $app["auth.cookie_expiration"] = "7day";
+
+        $app->register(new AuthenticatorServiceProvider());
+    }
+
+    /**
+     * @{inherit doc}
+     */
     public function boot(Application $app)
     {
-        $app->before([$this, "authBeforeFunction"], Application::EARLY_EVENT);
+        $app->before([$this, "authentication"], Application::EARLY_EVENT);
 
         $keys = [
             "auth.cookie_expiration",
@@ -39,20 +50,9 @@ class AuthServiceProvider implements ServiceProviderInterface, BootableProviderI
         $this->app = $app;
     }
 
-    /**
-     * @{inherit doc}
-     */
-    public function register(Container $app)
+    public function authentication(Request $req)
     {
-        $app["auth"]                   = $this;
-        $app["auth.cookie_expiration"] = "7day";
-
-        $app->register(new AuthenticatorServiceProvider());
-    }
-
-    public function authBeforeFunction(Request $req)
-    {
-        // On autorise les OPTIONS sans auth puisqu'il n'y a pas le cookie
+        // On autorise les OPTIONS sans authn puisqu'il n'y a pas le cookie
         if ('OPTIONS' === $req->getMethod()) {
             return;
         }
@@ -67,6 +67,7 @@ class AuthServiceProvider implements ServiceProviderInterface, BootableProviderI
             try {
                 $req->user = $this->app["authenticator"]->extract($req->cookies->get("authenticator"));
             } catch (Exception $e) {
+                // $this->app["logs"]->debug("");
                 $this->app->abort(401, "authenticator: cookie signature mismatch");
             }
 
@@ -80,6 +81,78 @@ class AuthServiceProvider implements ServiceProviderInterface, BootableProviderI
             $response->headers->clearCookie("authenticator", "/", $req->domain);
 
             return $response;
+        }
+    }
+
+    /**
+     * Vérifie si l'utilisateur courant a ce role
+     *
+     * @param string $roles : "adm"
+     * @return true
+     * @throws HttpException
+     */
+    public function hasRole($role)
+    {
+        return $this->hasRoles([$role]);
+    }
+
+    /**
+     * Vérifie si l'utilisateur courant a tous les roles
+     *
+     * @param array $roles : ["adm", "terminator"]
+     * @return true
+     * @throws HttpException
+     */
+    public function hasRoles(array $roles)
+    {
+        return function (Request $req, Response $res) {
+            foreach ($roles as $role) {
+                if (!in_array($role, $req->user->roles))) {
+                    $this->app->abort(403, "Not Authorised");
+                }
+            }
+
+            return true;
+        };
+    }
+
+    /**
+     * Vérifie si l'utilisateur courant a un ou plusieurs roles
+     *
+     * Dans l'example ci-dessous, ça passe si je suis "adm" OU ("profs" ET "paris")
+     *
+     * @param array $roles : ["adm", ["profs", "paris"]]
+     * @return true
+     * @throws HttpException
+     */
+    public function hasSomeRoles(array $roles)
+    {
+        return function (Request $req, Response $res) use ($roles) {
+            foreach ($roles as $role) {
+                try {
+                    if (is_array($role)) {
+                        return $this->hasRoles($role)
+                    } else {
+                        return $this->hasRole($role)
+                    }
+                } catch (\Exception $e) {
+                    // On fait rien puisqu'on est dans une boucle,
+                    // donc on va tenter les autres possibilités
+                }
+            }
+
+            // Et au pire, si on arrive ici, c'est que toutes les possibilités
+            // ont échoué, donc on peut dire que l'acces est interdit
+            $this->app->abort(403, "Not Authorised");
+        };
+    }
+
+    public function noLogas()
+    {
+        return function (Request $req, Response $res) {
+            if (!empty($req->user->logas)) {
+                $this->app->abort(403, "Not Authorised");
+            }
         }
     }
 }
